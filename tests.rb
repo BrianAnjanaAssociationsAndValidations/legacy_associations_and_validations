54d7e288a643425481bea391bdb7e850ec367ae3
anjanamohanty <anjana.j.mohanty@gmail.com>
2016-02-18 13:04:07 -0500

    Associate schools with terms (both directions)

--- tests.rb
+++ tests.rb
@@ -25,4 +25,32 @@ class ApplicationTest < Minitest::Test
     assert true
   end
 
+  # The tasks will be divided as follows. "Associate" means to place has_many, belongs_to, has_and_belongs_to_many, etc
+  # in the appropriate classes. "Validate" means to use validates in the appropriate classes with the appropriate parameters.
+
+  # Person A:
+
+  # Associate schools with terms (both directions).
+  def test_schools_are_associated_with_terms
+    school = School.create(name: "The Iron Yard")
+    term = Term.create(name: "Sprint 2016 Cohort", starts_on: "2016-02-01", ends_on: "2016-05-22")
+    term_two = Term.create(name: "Fall 2016 Cohort")
+
+    assert school.terms << term
+    assert school.terms << term_two
+
+    assert_equal 2, school.terms.count
+  end
+  # Associate terms with courses (both directions). If a term has any courses associated with it, the term should not be deletable.
+  # Associate courses with course_students (both directions). If the course has any students associated with it, the course should not be deletable.
+  # Associate assignments with courses (both directions). When a course is destroyed, its assignments should be automatically destroyed.
+  # Associate lessons with their pre_class_assignments (both directions).
+  # Set up a School to have many courses through the school's terms.
+  # Validate that Lessons have names.
+  # Validate that Readings must have an order_number, a lesson_id, and a url.
+  # Validate that the Readings url must start with http:// or https://. Use a regular expression.
+  # Validate that Courses have a course_code and a name.
+  # Validate that the course_code is unique within a given term_id.
+  # Validate that the course_code starts with three letters and ends with three numbers. Use a regular expression.
+
 end
