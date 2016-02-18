0aab16676dbd3d10b1cba7cf733feec5fde63c22
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-02-18 14:36:25 -0500

    lessons can have many readings.

--- tests.rb
+++ tests.rb
@@ -25,4 +25,14 @@ class ApplicationTest < Minitest::Test
     assert true
   end
 
+  def test_lessons_has_many_readings
+    lesson = Lesson.create(name:"Algerbra Basics", description: "Basic intro into the wonderful world of Algebra", outline: "See math, do math")
+    reading1 = Reading.create(caption: "Back to Basics", url: "http://stopfailingatmaths.com", order_number: 1)
+    reading2 = Reading.create(caption: "Linear Algebra", url: "http://sureyourereadyforthis.com", order_number: 2)
+
+    assert lesson.readings << reading1
+    assert lesson.readings << reading2
+
+    assert_equal 2, lesson.readings.count
+  end
 end
